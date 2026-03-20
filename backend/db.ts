34ad81747596acd98164888e5db22fc9ab5341e1
MReaufSG <krystiankubacki7@gmail.com>
2026-03-20 23:34:28 +0100

    Working prisma frfr

--- backend/db.ts
+++ backend/db.ts
@@ -1,10 +1,11 @@
+import { PrismaPostgresAdapter } from "@prisma/adapter-ppg";
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "./generated/prisma/client.js";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPostgresAdapter({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
+
--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,3 +1,4 @@
+// prisma.config.ts
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
@@ -5,7 +6,6 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "tsx prisma/seed.ts",
   },
   datasource: {
     url: env("DATABASE_URL"),
